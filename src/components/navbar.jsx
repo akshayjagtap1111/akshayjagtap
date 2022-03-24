2a92757c835a01b7c43feb445d97cad8c9c7a5a0
akshayjagtap1111 <93571686+akshayjagtap1111@users.noreply.github.com>
2022-03-24 22:12:55 +0530

    Update navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -134,7 +134,7 @@ class Navbar extends React.Component {
               </li>
               <li className="nav-item">
                 <a className="nav-link js-scroll" href="#work">
-                  Work
+                  Projects
                 </a>
               </li>
               <li className="nav-item">
